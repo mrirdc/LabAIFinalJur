605522cd5e317b70dd101a6560d1ba824265c008
mrirdc <mariaemilia.raducu@ulbsibiu.ro>
2026-06-14 23:28:38 +0300

    lab10

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 import pandas as pd
-from pandas import set_option
-
-data=pd.read_csv('data.csv')
-#pd.set_option('display.max_columns', None)
-#pd.set_option('display.max_rows', None)
-#print(data)
-
-#2
-jucatori_peste_40ani=data[data['Age']>40].head(10)
-print(jucatori_peste_40ani)
-
-#3
-jucatori_over_age=data[(data['Overall']>=85) & (data['Age']<25)]
-print(jucatori_over_age)
-
-#4
-data_sm=data.sort_values("Skill Moves",ascending=False).head(3)
-print(data_sm)
-
-#5
-jucatori_2021=data[data['Contract Valid Until']=='2021'].head()
-print(jucatori_2021)
+data=pd.read_csv('StudentsPerformance.csv')
 
 
+print(" ")
+print("ex1:")
+print(data.head)
+print(data.head())
+print (data.info())
+print(data.describe())
+print(data.isnull().sum())
+
+print("ex2:")
+numerice=data.select_dtypes(include=['number']).columns
+categorical=data.select_dtypes(include=['category']).columns
+print(numerice)
+print(categorical)
+
+print("ex3:")
+
+categorice = data.select_dtypes(include=['object', 'string']).columns
+
+for col in numerice:
+    mediana = data[col].median()
+    data[col] = data[col].fillna(mediana)
 
 
+for col in categorice:
+    data[col] = data[col].fillna("Unknown")
 
 
+print("--- Verificare finală valori lipsă ---")
+print(data.isnull().sum())
